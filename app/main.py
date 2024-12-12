10b2f89bf789a5032da8ee1fc411ec6f51983b12
Andreas <249964@usn.no>
2024-12-12 19:41:18 +0100

    Removed Credentials

--- app/main.py
+++ app/main.py
@@ -9,9 +9,9 @@ from keystoneauth1.identity import v3
 
 def NovaClient():
 
-    auth = v3.ApplicationCredential(auth_url='https://pegasus.sky.oslomet.no:5000/v3',
-                                    application_credential_id='bf1cb183404d4490ae0d04f3737c4c9a',
-                                    application_credential_secret='6stPHUnryXuL9Yo_3TWqVnm7jWodPYBJzv5SyUpXfPzm4ZCK0W739bH4QXuqHwIwMkBK6nGopXVTRDnTlfCbqg')
+    auth = v3.ApplicationCredential(auth_url='',
+                                    application_credential_id='',
+                                    application_credential_secret='')
     sess = session.Session(auth=auth)
     nova = novaclient.Client("2.0", session=sess)
     return nova
